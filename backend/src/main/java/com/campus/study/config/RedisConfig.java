09a2c166378b79d6de1e77a2687127fbf732e146
QDqiaodong <573717339@qq.com>
2026-06-09 20:22:04 +0800

    .2332509126274915:2cc0965169024729d4d6cd69d305553e_6a27f9e5f7e2379494009fa5.6a28029cf7e237949400a0c5.6a28029b58b5e953dd52d26d:Trae CN.T(2026/6/9 20:10:04)

--- backend/src/main/java/com/campus/study/config/RedisConfig.java
+++ backend/src/main/java/com/campus/study/config/RedisConfig.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.PropertyAccessor;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.jsontype.impl.LaissezFaireSubTypeValidator;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
@@ -25,6 +26,7 @@ public class RedisConfig {
         ObjectMapper objectMapper = new ObjectMapper();
         objectMapper.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
         objectMapper.activateDefaultTyping(LaissezFaireSubTypeValidator.instance, ObjectMapper.DefaultTyping.NON_FINAL);
+        objectMapper.registerModule(new JavaTimeModule());
         jackson2JsonRedisSerializer.setObjectMapper(objectMapper);
 
         template.setKeySerializer(stringRedisSerializer);
